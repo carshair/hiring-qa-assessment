7f14ca3ec86bd501663ec4be24426b7eb8d7f81d
Brian Hannay <brianh@carshair.com>
2022-03-25 11:41:33 -0700

    Add body to protected resource request so we can assess input validations

--- src/server/controllers/protected.ts
+++ src/server/controllers/protected.ts
@@ -1,6 +1,12 @@
-import { Get, JsonController } from "routing-controllers";
+import { BadRequestError, Body, JsonController, Post } from "routing-controllers";
 import fetch from "node-fetch";
 import { V1_API_DOMAIN } from "../../constants";
+
+interface ProtectedResourceBody {
+  operation: "SEND" | "REQUEST",
+  count: number,
+  text: string,
+};
 
 @JsonController("/protected")
 export default class ProtectedController {
@@ -9,9 +15,14 @@ export default class ProtectedController {
    *
    * @returns a protected resource.
    */
-  @Get()
-  async getProtected() {
-    const response = await fetch(`${V1_API_DOMAIN}/v1/protected`);
+  @Post()
+  async getProtected(@Body() body: ProtectedResourceBody) {
+    if (body.text?.length > 4096) {
+      throw new BadRequestError();
+    }
+    const response = await fetch(`${V1_API_DOMAIN}/v1/protected`, {
+      body: JSON.stringify(body),
+    });
     return response.json();
   }
 }
